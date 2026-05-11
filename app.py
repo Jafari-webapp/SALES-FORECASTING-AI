48e240988af863c19be28bcbf9141c705a146508
Jafari-webapp <jafarikitango@gmail.com>
2026-05-11 04:01:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ y = df["Total_Sales"]
 # =========================
 model = DecisionTreeRegressor()
 model.fit(X_train, Y_train)
+preds = model.predict(X_test)
 st.write("📉 Model Performance")
             st.write("R² Score:", round(r2_score(y_test, preds), 4))
             st.write("MSE:", round(mean_squared_error(y_test, preds), 4))
